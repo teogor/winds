346fd2cf75b5c4ea5d5625ee08f8c28dd739f4c0
Teodor Grigor <open-source@teogor.dev>
2023-11-08 12:32:21 +0200

    Resolved misconfiguration in modules using Kotlin DSL

--- demo-2/build.gradle.kts
+++ demo-2/build.gradle.kts
@@ -1,3 +1,20 @@
+import dev.teogor.winds.api.model.Version
+
 plugins {
+  `kotlin-dsl`
   id("dev.teogor.winds")
+}
+
+winds {
+  mavenPublish {
+    displayName = "Demo 2"
+    name = "demo-kotlin-dsl-tst2"
+    description = "Demo 2 Description set in here"
+
+    version = Version(
+      major = 1,
+      minor = 0,
+      patch = 0,
+    ).setAlphaRelease(1)
+  }
 }
