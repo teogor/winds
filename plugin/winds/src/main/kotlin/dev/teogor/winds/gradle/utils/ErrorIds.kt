0a956c6f7c9cf88e918ddb05c293e52ea8fc3294
Teodor Grigor <open-source@teogor.dev>
2023-11-08 11:57:58 +0200

    Added Error Identifier Constants in Utility Class

---
+++ plugin/winds/src/main/kotlin/dev/teogor/winds/gradle/utils/ErrorIds.kt
@@ -0,0 +1,14 @@
+package dev.teogor.winds.gradle.utils
+
+sealed interface ErrorId {
+  val errorId: Int
+
+  val errorIdFormatted: String
+    get() = "_eW$errorId"
+
+  fun getErrorIdString(): String = "Error ID: $errorIdFormatted"
+
+  data class GenericError(
+    override val errorId: Int = 1000
+  ) : ErrorId
+}
